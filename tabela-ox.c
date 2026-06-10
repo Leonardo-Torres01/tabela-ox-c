f977eda5bc30277b79cc3c61e8403766e87adcc4
Leonardo <leomatorres1901@gmail.com>
2026-06-10 20:17:29 -0300

    Update tabela-ox.c

--- tabela-ox.c
+++ tabela-ox.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function_lista.c"
+#include "function_tabela.c"
 
-int main(int argc, char *argv[]) 
-{
-    char lista[9] = {0};
+int main(int argc, char *argv[])
+ {
+    char tabela[3][3] = {{0}};
 
-
-    if (argc > 1)
-     {
-        preencher_por_argumentos(lista, argc, argv);
+    if (argc > 1) 
+    {
+        preencher_tabela_argumentos(tabela, argc, argv);
     } 
     else 
     {
-      
-        for (int i = 0; i < 9; i++) 
+        for (int i = 0; i < 3; i++) 
         {
-            char temp;
-            printf("Digite o caractere %d da lista: ", i + 1);
-            scanf(" %c", &temp);
-
-            while (!validar_caractere(temp)) 
+            for (int j = 0; j < 3; j++) 
             {
-                printf("caractere invalido\n");
-                printf("Digite o caractere %d da lista: ", i + 1);
+                char temp;
+                printf("Digite o caractere da linha %d coluna %d: ", i + 1, j + 1);
                 scanf(" %c", &temp);
+
+                while (!validar_caractere_tabela(temp)) 
+                {
+                    printf("caractere invalido\n");
+                    printf("Digite o caractere da linha %d coluna %d: ", i + 1, j + 1);
+                    scanf(" %c", &temp);
+                }
+                tabela[i][j] = temp;
             }
-            lista[i] = temp;
         }
     }
-
-   
-    for (int i = 0; i < 9; i++) 
+    for (int i = 0; i < 3; i++) 
     {
-        printf("Caractere %d: %c\n", i + 1, lista[i]);
+        for (int j = 0; j < 3; j++) 
+        {
+            printf("Linha %d Coluna %d: %c\n", i + 1, j + 1, tabela[i][j]);
+        }
     }
 
     printf("\nFeito por Leonardo Mattoso (https://github.com/Leonardo-Torres01)");
