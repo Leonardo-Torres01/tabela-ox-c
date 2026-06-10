dfaf3c3c2392235f5fbac225317ac3aeb2a3ad5a
Leonardo <leomatorres1901@gmail.com>
2026-06-10 20:15:29 -0300

    Update function_tabela.c

--- function_tabela.c
+++ function_tabela.c
@@ -8,22 +8,22 @@ int validar_caractere_tabela(char c)
 void preencher_tabela_argumentos(char tabela[3][3], int argc, char *argv[]) 
 {
     int l = 0, c = 0;
-    for (int i = 1; i < argc; i++)
-{
-    for (int j = 0; argv[i][j] != '\0'; j++) 
-{
-    char caractere = argv[i][j];
-    if (validar_caractere_tabela(caractere)) 
-{
-    tabela[l][c] = caractere;
-      c++;
-    if (c == 3) 
- {
-     c = 0;
-    l++;
-}
-    if (l == 3) return; 
-}
-}
-}
+    for (int i = 1; i < argc; i++) 
+    {
+        for (int j = 0; argv[i][j] != '\0'; j++) 
+        {
+            char caractere = argv[i][j];
+            if (validar_caractere_tabela(caractere)) 
+            {
+                tabela[l][c] = caractere;
+                c++;
+                if (c == 3) 
+                {
+                    c = 0;
+                    l++;
+                }
+                if (l == 3) return; 
+            }
+        }
+    }
 }
